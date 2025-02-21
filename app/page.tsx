77f5a1d69347c98d77d66f551445467ec6d77459
ParsaProg <parsashaabani3@gmail.com>
2025-02-21 14:54:34 +0330

    add timer icon to start button

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import { LuTimer } from "react-icons/lu";
+
 
 export default function App() {
   return (
-    <div className="app bg-black text-white h-[100vh] justify-center items-center flex flex-col text-3xl">
+    <div className="app text-center bg-black text-white h-[100vh] justify-center items-center flex flex-col text-3xl">
       Parsa Projects in the web
       <Link href={"/timer"}>
-        <button className="bg-white text-black rounded-md mt-[20px] px-3 py-2 text-lg hover:text-white border-[1px] border-white transition-all duration-200 hover:bg-transparent">
+        <button className="flex items-center justify-center bg-white text-black rounded-md mt-[20px] px-3 py-2 text-lg hover:text-white border-[1px] border-white transition-all duration-200 hover:bg-transparent">
           Timer Project
+          <LuTimer size={20}/>
         </button>
       </Link>
     </div>
